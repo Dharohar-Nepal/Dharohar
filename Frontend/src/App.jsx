b4bf64fb4f92b2ab9fe9c7d390d2260ac6ce521b
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-07-26 00:46:58 +0545

    Some more updates

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Explore from './pages/Explore'
+import Articles from './pages/Articles'
 
 export default function 
 () {
   return (
     <>
-      Hello
+    <Router >
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/contact' element= { <Element />} />
+        <Route path='/explore' element= { <Explore />} />
+        <Route path='/article' element= { <Articles />} />
+      </Routes>
+    </Router>
+      
     </>
   )
 }
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Category from '../components/Category'
+import Testimonials from '../components/Testimonials'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <Header />
+        <Category />
+        <Testimonials />
+        <Footer />
+    </div>
+  )
+}
