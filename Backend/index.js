dd30ed553844de512f1d8bc0adad5d9e3800f1ca
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-07-25 23:46:47 +0545

    Updates

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,15 @@ import express from 'express'
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    try {
+        res.send('This is my project')
+        
+    } catch (error) {
+        console.log('Error: error.message')
+    }
+})
+
 app.listen(port, () => {
     console.log(`Listening to ${port}`)
 })
