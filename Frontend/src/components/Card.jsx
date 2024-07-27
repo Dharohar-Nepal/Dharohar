a5f77b155b2db25d40866f275644694752a76605
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-07-28 01:15:32 +0545

    Some more updates

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,39 +1,31 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/context';
+import React from "react";
 
-export default function Card() {
-    const { item } = useContext(AppContext);
-
-    return (
-        <>
-            <div className='flex justify-center items-center flex-wrap gap-5 '>
-                {
-                    item.map((value) => {
-                        return (
-                            <div>
-                                <div key={value.id} className="card bg-base-100 min-w-72 w-96 max-h-96  overflow-hidden p-10 relative shadow-xl hover:opacity-50 hover:transition-all delay-75">
-                                    <figure>
-                                        <img
-                                            src={value.img}
-                                            alt="Shoes" />
-                                    </figure>
-                                    <div className="card-body">
-                                        <h2 className="card-title">{value.place}</h2>
-                                        <p>{value.description_english}</p>
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Translate Into Nepali</button>
-                                        </div>
-                                    </div>
-                                    <button className='hover:absolute hover:opacity-100 grid items-center'>Readmore</button>
-
-                                </div>
-
-
-                            </div>
-                        );
-                    })
-                }
+export default function Card({ items }) {
+  return (
+    <>
+      <div className="flex justify-center items-center flex-wrap gap-5">
+        {items.map((value) => (
+          <div key={value.id}>
+            <div className="card bg-base-100 min-w-72 w-96 max-h-96 overflow-hidden p-10 relative shadow-xl hover:opacity-50 hover:transition-all delay-75">
+              <figure>
+                <img src={value.img} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{value.place}</h2>
+                <p>{value.description_english}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">
+                    Translate Into Nepali
+                  </button>
+                </div>
+              </div>
+              <button className="hover:absolute hover:opacity-100 grid items-center">
+                Readmore
+              </button>
             </div>
-        </>
-    );
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- Frontend/src/components/Category.jsx
+++ Frontend/src/components/Category.jsx
@@ -3,33 +3,29 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/context';
 
 export default function Category() {
-
   const { item } = useContext(AppContext);
-  const [filter, setFilter] = useState(item)
+  const [filter, setFilter] = useState(item);
 
   const filteredData = (category) => {
-
-    const filteredItems = item.filter((p) => p.category === 'category'      
-      setFilter(filteredItems)
-    )
-  }
-  console.log(filteredData)
+    const filteredItems = item.filter((p) => p.category === category);
+    setFilter(filteredItems);
+  };
 
   return (
     <>
       <div className='my-10'>
         <div>
           <ul className='flex justify-center items-center gap-5 cursor-pointer'>
-            <li>Natural</li>
-            <li>Religious</li>
-            <li>Historical</li>
+            <li onClick={() => filteredData('Natural Destination')}>Natural</li>
+            <li onClick={() => filteredData('Religious Destination')}>Religious</li>
+            <li onClick={() => filteredData('Historical Destination')}>Historical</li>
           </ul>
         </div>
 
         <div>
-          <Card />
+          <Card items={filter} />
         </div>
       </div>
     </>
-  )
+  );
 }
