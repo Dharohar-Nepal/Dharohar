3f6d20a9533459140a18934759e13f8d3c63be10
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-08-13 00:40:24 +0545

    Some more updates

--- Frontend/src/context/context.jsx
+++ Frontend/src/context/context.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import Data from '../Data'
+import Data from '../Data' //fetched data from file
 
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-    const [item, setItem] = useState(Data)
+    const [item, setItem] = useState(Data) //for state management
 
-    return <AppContext.Provider value={{ item, setItem }} >
+
+
+    //passing the values obtained from data in the name of item to any of the component 
+    return <AppContext.Provider value={{ item, setItem }} > 
         {
             children
         }
